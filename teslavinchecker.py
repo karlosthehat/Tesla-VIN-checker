49ce1eb712cbab38e590c1e5e01bcacdb6b2f268
karlosthehat <98567830+karlosthehat@users.noreply.github.com>
2022-02-22 09:20:53 +1100

    Update teslavinchecker.py
    New code

--- teslavinchecker.py
+++ teslavinchecker.py
@@ -1,27 +1,40 @@
 from selenium import webdriver
-import yagmail
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime
 import os
 import sys
-options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=/home/pi/.config/chromium')
-options.add_argument('--profile-directory=Default')
-teslaaccount = "http://www.tesla.com/teslaaccount"
-profile = "http://www.tesla.com/teslaaccount/profile?rn=RNxxxxxxxxx" #put your RN here
+profile = "http://www.tesla.com/teslaaccount/profile?rn=RNxxxxxxxxx" #Put your RN here
 logout = "http://www.tesla.com/user/logout/"
-driver = webdriver.Chrome(options=options)
-driver.get(teslaaccount)
-python_button = driver.find_element_by_id('form-submit-continue')
-python_button.click()
-driver.get(profile)
-if "LRW" in driver.page_source: #you might need to change this to whatever prefixes your VIN
-    yag = yagmail.SMTP(‘youremail@address.com’, ‘password’) #put your email address and password here so yagmail can use it.
-    yag.send(‘addressyouwant@toemail.com’, 'TESLA VIN ALERT', "Your VIN has been found") #put the email address you want to send a notification to here
+driver = webdriver.Chrome()
+
+def check_vin():
+    print("Checking for a VIN...")
+    driver.get(profile)
+    username = driver.find_element(By.ID, 'form-input-identity')
+    password = driver.find_element(By.ID, 'form-input-credential')
+    username.send_keys('xxx') #put your Tesla username here
+    password.send_keys('xxx') #put your Tesla password here
+    login_button = driver.find_element(By.ID, 'form-submit-continue')
+    login_button.click()
+    if "LRW" in driver.page_source: #Change "LRW" as necessary
+        vin_found()
+    else:
+        vin_notfound()
+
+def vin_found():
+    print("VIN found in source code at", datetime.now())
+    print("Program closing.")
     driver.get(logout)
     driver.quit()
     sys.stdout.flush()
     os._exit(0)
-else:
+
+def vin_notfound():
+    print("No VIN yet :( Checking again in 1 hour.")
     driver.get(logout)
-    driver.quit()
-    sys.stdout.flush()
-    os._exit(0)
+    time.sleep(3600) #Time in seconds determines how often you want to check for a VIN. I recommend no more than every hour
+    check_vin()
+
+check_vin()
